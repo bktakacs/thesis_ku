1dfb19cf15be5f1c1152b05c9e18ff023adc0f60
Bence Takacs <dtn125@alumni.ku.dk>
2023-08-03 19:30:08 +0100

    none

--- run.py
+++ run.py
@@ -10,84 +10,102 @@ def main():
 
     if __name__ == '__main__':
 
+        m = chi_search(
+            'new-friedmann-eq30-bdf-extended.txt',
+            length=50, blim=(1, 4), klim=(0.01, 1000), 
+        )
+        m.plot('acc')
+        m.distance_modulus()
+        m.plot('dm')
+
         # b = np.linspace(1, 4, 50)
         # # k = np.linspace(0.1, 1000, 51)
         # # k = np.logspace(-1, 3, 50)
         # k = np.geomspace(0.1, 1000, 50)
 
-        # data = read_model_data('new-friedmann-before30.txt')
+        # data = read_model_data('new-friedmann-eq30-bdf-1.txt')
         # b, k = specific_function(data, 0)
         # m = model(beta=b, kappa=k, lam=0.)
-        # m.plot('acc')
         # m.distance_modulus()
-        # m.plot('dm')
+        # # print(b, k)
+        # # m.plot('acc')
+        # # m.distance_modulus()
+        # # m.plot('dm')
 
-        # brange = np.unique(np.array(data['beta']))
-        # krange = np.unique(np.array(data['kappa']))
-        # length = int(np.sqrt(len(chival)))
-        # acc = False
-        # round = 1
 
-        # chi_plot_z = np.reshape(chival, (length, length)).T
+        # m1 = model(beta=1.979591836734693855, kappa=15.99858719606057278, lam=0.)
+        # m2 = model(beta=1.612244897959183687, kappa=86.85113737513520960, lam=0.)
+        # m3 = model(beta=2.224489795918367374, kappa=10.98541141987558412, lam=0.)
 
-        # fig, ax = plt.subplots(figsize=(7, 5), constrained_layout=True)
-        # ax.set_yscale('log')
-        # cmap = matplotlib.cm.get_cmap('viridis_r').copy()
-        # cmap.set_bad(color='w')
-        # # im = ax.imshow(chi_plot_z, cmap=cmap, origin='lower',
-        # #                interpolation='nearest',
-        # #                norm=scale,
-        # #                )
-        # im = ax.pcolormesh(
-        #     b, k, chi_plot_z, norm=None, edgecolors='w',
-        #     cmap='viridis_r'
+        # m1.distance_modulus()
+        # m2.distance_modulus()
+        # m3.distance_modulus()
+
+        # fig = plt.figure(constrained_layout=False)
+        # frame1 = fig.add_axes((.1, .3, .8, .6))
+        # plt.errorbar(
+        #     z_sn, df['m_b_corr'], yerr=snerr, lw=0.5, ls='', marker='.',
+        #     markersize=2, label=r'Pantheon+SH0ES', zorder=0
         # )
-        # index = np.argmin(np.abs(k - kappa_low))
-        # highlight_cell(
-        #     beta_low-0.5*np.diff(b)[0],
-        #     kappa_low-0.5*(k[index] - k[index-1]),
-        #     np.diff(b)[0],
-        #     (k[index] - k[index-1]),
-        #     ax=ax, color='k', linewidth=1
+
+        # mb_astro, _ = mbcorr_fit(dm_astro)
+        # plt.plot(
+        #     z_sn, mb_astro, c='orange', ls='-',
+        #     label=r'$\Lambda$CDM',
         # )
-        # ax.set_xlabel(r'$\beta$')
-        # ax.set_ylabel(r'$\mu$')
-        # fig.colorbar(im, ax=ax, label=r'$\chi^{{2}}_{{r, {}}}$'.format(
-        #     '\ddot{a}/\ddot{a}_{\mathrm{M}}' if acc
-        #     else 'E(z)'
-        # ))
-        # ax.tick_params(
+        
+        # plt.plot(
+        #     z_sn, m.mb_int, c='k', ls='-.',
+        #     label=r'Alt. Model, $\beta = {:.2f}$'.format(m.b),
+        # )
+
+        # # plt.plot(
+        # #     z_sn, m2.mb_int, c='r', ls='--',
+        # #     label=r'Model 2',
+        # # )
+
+        # # plt.plot(
+        # #     z_sn, m3.mb_int, c='b', ls=':',
+        # #     label=r'Model 3',
+        # # )
+
+
+        # # plt.plot(
+        # #     z_sn, m2.mb_int, c='r', ls='--',
+        # #     label=r'$\beta=1.98, \mu = $',
+        # # )
+        
+        # plt.ylabel(r'$m_{B}$ [mag]')
+        # plt.xscale('log')
+        # plt.tick_params(axis='both', which='both', direction='in',
+        #                 bottom=True, top=True, left=True, right=True)
+        # plt.legend()
+        # plt.grid()
+        # plt.ylim([7, 28])
+        # frame1.set_xticklabels([])
+
+        # frame2 = fig.add_axes((.1, .1, .8, .2))
+        # plt.errorbar(
+        #     z_sn, df['m_b_corr'] - mb_astro, yerr=snerr, lw=0.5, ls='',
+        #     marker='.', markersize=2, label=r'Supernova',zorder=0
+        # )
+        # plt.plot(z_sn, (m.mb_int - mb_astro), c='k', ls='-.')
+        # # plt.plot(z_sn, (m2.mb_int - mb_astro), c='r', ls='--')
+        # # plt.plot(z_sn, (m3.mb_int - mb_astro), c='b', ls=':')
+
+        # plt.xlabel(r'$z$')
+        # plt.xscale('log')
+        # plt.ylabel(r'$\Delta{m_{B}}_{\Lambda\mathrm{CDM}}$')
+        # plt.tick_params(
         #     axis='both', which='both', direction='in',
         #     bottom=True, top=True, left=True, right=True
         # )
+        # plt.tick_params(axis='x', pad=7)
+        # # extraticks = [-1, 1]
+        # # plt.yticks(list(plt.yticks()[0]) + extraticks)
+        # # plt.ylim([-2, 2])
+        # plt.ylim([-1, 1])
+        # plt.grid(which='major', axis='both')
         # plt.show()
-
-        pass    
-
-        # m1 = chi_search(
-        #     # 'nosave', length=50, blim=(1, 4), klim=(0.1, 10), solver='BDF',
-        #     'new-friedmann-pre30-mult-a-1', length=50, blim=(1, 4), klim=(0.1, 1000),
-        #     solver='BDF', acc=False,
-        # )
-        # m1.plot('acc')
-        # m1.distance_modulus()
-        # m1.plot('dm')
-
-        # m2 = chi_search(
-        #     'new-friedmann-eq30-rk45-1', length=50, blim=(1, 4), klim=(0.1, 1000),
-        #     solver='RK45', acc=False,
-        # )
-        # m2.plot('acc')
-        # m2.distance_modulus()
-        # m2.plot('dm')
-
-
-        # m = model(beta=1.979591836734693855, kappa=15.99858719606057278, lam=0.)
-        # m = model(beta=1.612244897959183687, kappa=86.85113737513520960, lam=0.)
-        # m = model(beta=2.224489795918367374, kappa=10.98541141987558412, lam=0.)
-        # m = model()
-        # m.plot('acc')
-        # m.distance_modulus()
-        # m.plot('dm')
 
 main()
